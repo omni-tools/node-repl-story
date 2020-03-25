1aee8d74e0d44a23ac0b30104a7ecd66fc718227
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-03-25 22:32:45 +0100

    Prototype writing of history :memo:

--- src/repl-history.js
+++ src/repl-history.js
@@ -1,23 +1,53 @@
 const {REPLServer} = require('repl');
 const fs = require('fs');
 
-const loadHistoryIntoReplServer = (replServer, historyFile) => {
-  const history = fs
-    .readFileSync(historyFile)
-    .toString()
-    .split('\n'); // TODO: better parsing
-  replServer.history = history;
+const BASIC_WHITELIST = []; // TODO: make it configurable!
 
+const setUpHistory = (replServer, filename) => {
+  loadHistoryIntoReplServer(replServer, filename);
+  setUpHistoryRecording(replServer, filename); // todo: make it optional
+
+  // TODO: commands and more!
   return replServer;
+};
+const loadHistoryIntoReplServer = (replServer, filename) => {
+  const history = fs
+    .readFileSync(filename)
+    .toString()
+    .split('\n')
+    .reverse()
+    .slice(1);
+  // TODO: better parsing
+  replServer.history = history;
+  return replServer;
+};
+
+const setUpHistoryRecording = (replServer, filename) => {
+  const descriptor = fs.openSync(filename, 'a');
+  const scribe = fs.createWriteStream(filename, {fd: descriptor});
+
+  replServer.on('line', cmd => {
+    if (cmd && !BASIC_WHITELIST.includes(cmd)) {
+      scribe.write(`${cmd}\n`);
+    } else {
+      // erase item from history
+      replServer.historyIndex++;
+      replServer.history.pop();
+    }
+  });
+  process.on('exit', function() {
+    fs.closeSync(descriptor);
+  });
 };
 
 const replHistory = options => {
   const {replServer, repl = replServer, historyFile, filename = historyFile, prompt} =
     options || {};
   // TODO: check historyFile exists
-  if (repl instanceof REPLServer) return loadHistoryIntoReplServer(repl, filename);
+  // MAYBE: read only mode, injectable values
+  if (repl instanceof REPLServer) return setUpHistory(repl, filename);
 
-  return loadHistoryIntoReplServer(repl.start(prompt), filename);
+  return setUpHistory(repl.start(prompt), filename);
 };
 
 module.exports = replHistory;
